eb78448c099412b2a3eefc74a95d21a6c2cce27d
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-05-14 06:46:54 -0700

    Delete components directory

--- components/Code.jsx
+++
@@ -1,9 +0,0 @@
-const Code = ({ children, language = 'javascript' }) => {
-    return (
-        <div className="overflow-x-auto">
-            <pre className={`text-slate-200 bg-slate-700 w-50 rounded-md p-4 font-mono text-lg language-${language}`}>
-                <code>{children}</code>
-            </pre>
-        </div>
-    )
-}
