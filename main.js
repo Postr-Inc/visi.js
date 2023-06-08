bc51134faa90b7e3e97e8dec83b2a8e5d80e2f96
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-06-08 14:18:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,96 +1,25 @@
- 
-
-
 const app = new ReactRouter()
-window.app = app
-lib("@tailwind/daisyui@3.0.20")
-app.bindRoot("app")
+lib("@tailwind/core")
+// specify a route
+app.use('/helloworld')
 app.use('/')
-app.use('/team')
-app.use('/releases')
-app.use('/docs')
-updateCacheVersion(1)
+app.bindRoot("app")
+
+// set root initial route
 app.root("/", (req, res) =>{
     dispose('./views/main.jsx', (Main) =>{
-        res.title("Home")
+        res.jsx(<Main />)
         res.return()
-        res.jsx(<Main/>)
-      
     })
 })
-
-app.on('/team', (req, res)=>{
-    dispose('./views/team.jsx', (Team) =>{
-        res.title("Team")
-        res.return()
-        res.jsx(<Team/>)
-    })
-})
-
-app.on('/', (req, res)=>{
-    dispose('./views/main.jsx', (Main) =>{
-        res.title("Home")
-        res.return()
-        res.jsx(<Main/>)
-    })
-})
-
-app.on("/releases", (req, res) =>{
-    dispose('./views/releases.jsx', (Releases) =>{
-        res.title("Releases")
-        res.return()
-        res.jsx(<Releases/>)
-    })
-})
-
-app.get('/releases', (req, res) =>{
-    dispose('./views/releases.jsx', (Releases) =>{
-        res.title("Releases")
-        res.return()
-        res.jsx(<Releases/>)
-    })
-})
-app.get('/team', (req, res) =>{
-    dispose('./views/team.jsx', (Team) =>{
-        res.title("Team")
-        res.return()
-        res.jsx(<Team/>)
-    })
-})
-app.get('/', (req, res) =>{
-    dispose('./views/main.jsx', (Main) =>{
-        res.title("Home")
-        res.return()
-        res.jsx(<Main/>)
-    })
-})
-
-app.get('/docs/:lang/:page', (req, res) =>{
-    let page = req.params.page
-    dispose(`./views/docs/docs.jsx`, (Docs) =>{
-        res.title("Docs")
-        res.return()
-        res.jsx(<Docs/>)
-    },{
-        page: page,
-        lang: req.params.lang
-    
-    })
-})
-
-app.on('/docs/:lang/:page', (req, res) =>{
-    let page = req.params.page
-    dispose(`./views/docs/docs.jsx`, (Docs) =>{
-        res.title(page)
-        res.return()
-        res.jsx(<Docs/>)
-    },{
-        page: page,
-        lang: req.params.lang
-    
-    })
+// bind actions to the route
+app.get('/helloworld', (req, res) =>{
+    res.send("Hello World")
     res.return()
 })
-app.error((res) =>{
+ 
+// if route not found return 404
+app.error((res)=>{
     res.send("404")
+    res.return()
 })
